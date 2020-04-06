1f983d5cacdbad6727415fb767e8ac5fb6b925bf
fbzr <fabricio.spb@gmail.com>
2020-04-06 10:26:40 -0700

    create posts get and post requests

--- routes/posts.js
+++ routes/posts.js
@@ -1,16 +1,35 @@
 const express = require('express');
 const router = express.Router();
 
+const db = require('../data/db');
+
 // @route   GET /api/posts
 // @desc    Returns an array of all the post objects contained in the database.
-router.get('/', (req, res) => {
-
+router.get('/', async (req, res) => {
+    try {
+        const posts = await db.find();
+        res.json(posts);
+    } catch(err) {
+        res.status(500).json({ error: 'The posts information could not be retrieved.'});
+    }
 });
 
 // @route   POST /api/posts
 // @desc    Creates a post using the information sent inside the request body.
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
+    try {
+        const { title, contents } = req.body;
 
+        if (!title || !contents) {
+            return res.status(400).json({ errorMessage: 'Please provide title and contents for the post.' });
+        }
+
+        const response = await db.insert({ title, contents });
+        const createdPost = await db.findById(response.id);
+        res.status(201).json(createdPost[0]);
+    } catch(err) {
+        res.status(500).json({ error: 'There was an error while saving the post to the database' });
+    }
 });
 
 // @route   GET /api/posts/:id
