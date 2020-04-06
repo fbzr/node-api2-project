a64650142fe2102a443a338db34c05c332c87946
fbzr <fabricio.spb@gmail.com>
2020-04-06 10:47:08 -0700

    implement delete post route

--- routes/posts.js
+++ routes/posts.js
@@ -49,8 +49,18 @@ router.get('/:id', async (req, res) => {
 
 // @route   DELETE /api/posts/:id
 // @desc    Removes the post with the specified id and returns the deleted post object. 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
+    try {
+        const post = await db.findById(req.params.id);
+        if (post.length === 0) {
+            return res.status(404).json({ message: 'The post with the specified ID does not exist.' });
+        }
 
+        await db.remove(post[0].id);
+        res.json(post[0]);
+    } catch(err) {
+        res.status(500).json({ error: 'The post could not be removed.' });
+    }
 });
 
 // @route   PUT /api/posts/:id
